267e1d3a48c9795a4294276555a8b80565590c4d
wangzhen0518 <wangzhen0518@126.com>
2025-07-01 17:49:42 +0800

    deal multi command

--- app/main.js
+++ app/main.js
@@ -2,8 +2,10 @@ const net = require("net");
 
 const server = net.createServer(connection => {
     // Handle connection
-    console.log("connection :>> ", connection);
-    connection.write("+PONG\r\n");
+    connection.on("data", data => {
+        console.log(data.toString().split("\n"));
+        connection.write("+PONG\r\n");
+    });
 });
 
 server.listen(6379, "127.0.0.1");
