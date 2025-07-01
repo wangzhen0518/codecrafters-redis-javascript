ebd196cab07eaa3f6a4d17160bd1a9366b633a57
wangzhen0518 <wangzhen0518@126.com>
2025-07-01 18:21:01 +0800

    implement ECHO command

--- app/main.js
+++ app/main.js
@@ -1,10 +1,27 @@
 const net = require("net");
 
+function decodeCommand(commandString) {}
+
 const server = net.createServer(connection => {
     // Handle connection
     connection.on("data", data => {
-        console.log(data.toString().split("\n"));
-        connection.write("+PONG\r\n");
+        let commandList = data
+            .toString()
+            .split("\r\n")
+            .map(s => s.trim().toLowerCase())
+            .filter(s => s.length > 0 && !s.startsWith("*") && !s.startsWith("$"));
+        switch (commandList[0]) {
+            case "ping":
+                connection.write("+PONG\r\n");
+                break;
+            case "echo":
+                let respString = commandList.slice(1).join("\r\n");
+                respString = `\$${respString.length}\r\n${respString}\r\n`;
+                connection.write(respString);
+                break;
+            default:
+                break;
+        }
     });
 });
 
