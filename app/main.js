561dd4c6c83497fc12e176daece066f420381e10
wangzhen0518 <wangzhen0518@126.com>
2025-07-01 17:37:43 +0800

    naive response PING

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 const net = require("net");
 
-const server = net.createServer((connection) => {
-  // Handle connection
+const server = net.createServer(connection => {
+    // Handle connection
+    console.log("connection :>> ", connection);
+    connection.write("+PONG\r\n");
 });
 
 server.listen(6379, "127.0.0.1");
