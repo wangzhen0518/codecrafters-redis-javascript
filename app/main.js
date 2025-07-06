b6996297c7c598bd184317527cc07a8a96a0cccf
wangzhen0518 <wangzhen0518@126.com>
2025-07-06 10:19:00 +0800

    add support of redis client commands

--- app/main.js
+++ app/main.js
@@ -1,5 +1,15 @@
 const net = require("net");
 
+class Redis {
+    constructor() {
+        this.libName = "";
+        this.libVer = "";
+    }
+}
+
+const redis = new Redis();
+
+// TODO 实现完整的 parser
 function decodeCommand(commandString) {
     let commandList = commandString
         .toString()
@@ -21,6 +31,19 @@ function encodeStringResponse(stringResponse) {
         encodedString = "$-1\r\n";
     }
     return encodedString;
+}
+
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function lowercaseFirstLetter(str) {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+}
+
+function formatKeyName(attrName) {
+    let attrNameList = attrName.split("-").map(s => capitalizeFirstLetter(s.toLowerCase()));
+    return lowercaseFirstLetter(attrNameList.join(""));
 }
 
 function setKeyExpire(key, time) {
@@ -64,6 +87,30 @@ const server = net.createServer(connection => {
                 }
                 let resp = encodeStatusResponse("OK");
                 connection.write(resp);
+                break;
+            }
+            case "client": {
+                switch (commandList[1].toLowerCase()) {
+                    case "info": {
+                        let resp =
+                            "id=10 addr=127.0.0.1:39422 laddr=127.0.0.1:6379 fd=24 name= age=0 idle=0 flags=N db=0 sub=0 psub=0 ssub=0 multi=-1 watch=0 qbuf=26 qbuf-free=20448 argv-mem=10 multi-mem=0 rbs=16384 rbp=16384 obl=0 oll=0 omem=0 tot-mem=37786 events=r cmd=client|info user=default redir=-1 resp=2 lib-name= lib-ver= io-thread=0\n";
+                        resp = encodeStringResponse(resp);
+                        connection.write(resp);
+                        break;
+                    }
+                    case "setinfo": {
+                        for (let index = 2; index < commandList.length; index += 2) {
+                            const attrName = formatKeyName(commandList[index]);
+                            const attrValue = commandList[index + 1];
+                            redis[attrName] = attrValue;
+                        }
+                        let resp = encodeStatusResponse("OK");
+                        connection.write(resp);
+                        break;
+                    }
+                    default:
+                        break;
+                }
                 break;
             }
             default:
